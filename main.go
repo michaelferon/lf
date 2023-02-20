d61dc939e96c3f45448de9df8fbf821b114c7411
Michael Feron <feron.michael@gmail.com>
2023-02-20 01:16:09 -0700

    Add OLD_PWD environment variable.

--- main.go
+++ main.go
@@ -72,6 +72,7 @@ func exportEnvVars() {
 		fmt.Fprintf(os.Stderr, "getting current directory: %s\n", err)
 	}
 	os.Setenv("OLDPWD", dir)
+	os.Setenv("OLD_PWD", dir)
 
 	level, err := strconv.Atoi(envLevel)
 	if err != nil {
